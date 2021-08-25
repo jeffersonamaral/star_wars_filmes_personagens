7047dae9eebea15269fe2761423ab55bd40fb0ac
jeffersonamaral <jefferson.amarals@gmail.com>
2021-08-25 18:43:47 -0300

    Adição de tratamento para o evento de mudança de aba.

--- lib/view/widget/home.dart
+++ lib/view/widget/home.dart
@@ -6,77 +6,112 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+
+  late TabController _tabController;
+
+  String _title = 'Lista de Filmes';
+
+  @override
+  void initState() {
+    super.initState();
+
+    _tabController = TabController(
+        length: 3,
+        vsync: this
+    )..addListener(() {
+      setState(() {
+        switch (_tabController.index) {
+          case 0:
+            _title = 'Lista de Filmes';
+            break;
+          case 1:
+            _title = 'Lista de Personagens';
+            break;
+          case 2:
+            _title = 'Lista de Favoritos';
+            break;
+        }
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-              title: Text('Lista de Filmes'),
-              bottom: PreferredSize(
-                  preferredSize: Size(double.infinity, kToolbarHeight * 2),
-                  child: Padding(
-                      padding: EdgeInsets.all(10),
-                      child:
-                      Column(
+    return Scaffold(
+      appBar: AppBar(
+          title: Text(_title),
+          bottom: PreferredSize(
+              preferredSize: Size(double.infinity, kToolbarHeight * 2),
+              child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child:
+                  Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              ElevatedButton(
-                                  onPressed: () {
-                                    Navigator.of(context).pushNamed(RouteGenerator.officialSite);
-                                  },
-                                  style: ButtonStyle(
-                                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                                          RoundedRectangleBorder(
-                                              borderRadius: BorderRadius.circular(18.0),
-                                              side: BorderSide(
-                                                  width: .5
-                                              )
+                          ElevatedButton(
+                              onPressed: () {
+                                Navigator.of(context).pushNamed(RouteGenerator.officialSite);
+                              },
+                              style: ButtonStyle(
+                                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                      RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(18.0),
+                                          side: BorderSide(
+                                              width: .5
                                           )
                                       )
-                                  ),
-                                  child: Text('Site Oficial',
-                                    style: TextStyle(
-                                        fontSize: 18
-                                    ),
                                   )
                               ),
-                              InkWell(
-                                onTap: () {
-                                  Navigator.of(context).pushNamed(RouteGenerator.avatarEditing);
-                                },
-                                child: CircleAvatar(
-                                  radius: 25,
-                                  child: Image.asset('assets/images/no_avatar.png',
-                                    fit: BoxFit.fill,
-                                  ),
+                              child: Text('Site Oficial',
+                                style: TextStyle(
+                                    fontSize: 18
                                 ),
                               )
-                            ],
                           ),
-                          TabBar(
-                            tabs: [
-                              Tab(text: 'Filmes',),
-                              Tab(text: 'Personagens',),
-                              Tab(text: 'Favoritos',),
-                            ],
+                          InkWell(
+                            onTap: () {
+                              Navigator.of(context).pushNamed(RouteGenerator.avatarEditing);
+                            },
+                            child: CircleAvatar(
+                              radius: 25,
+                              child: Image.asset('assets/images/no_avatar.png',
+                                fit: BoxFit.fill,
+                              ),
+                            ),
                           )
                         ],
+                      ),
+                      TabBar(
+                        controller: _tabController,
+                        tabs: [
+                          Tab(text: 'Filmes',),
+                          Tab(text: 'Personagens',),
+                          Tab(text: 'Favoritos',),
+                        ],
                       )
+                    ],
                   )
               )
-          ),
-          body: TabBarView(
-            children: [
-              Text('Filmes'),
-              Text('Personagens'),
-              Text('Favoritos'),
-            ],
-          ),
-        )
+          )
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          Text('Filmes'),
+          Text('Personagens'),
+          Text('Favoritos'),
+        ],
+      ),
     );
   }
 }
